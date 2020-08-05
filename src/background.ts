dedc3a5a2199a9713563de968a89b012a0646be4
fiahfy <d.fiahfy@gmail.com>
2020-08-05 09:10:01 +0900

    Update background.ts

--- src/background.ts
+++ src/background.ts
@@ -3,7 +3,6 @@ import inject from '~/assets/inject.css'
 
 const contentLoaded = async (tabId: number) => {
   await browser.tabs.insertCSS(tabId, { file: inject })
-  await browser.pageAction.show(tabId)
 }
 
 browser.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
