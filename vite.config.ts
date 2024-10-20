3c4544fc3578bed406da190fb7614e0496b29303
fiahfy <d.fiahfy@gmail.com>
2024-10-20 23:11:05 +0900

    Upgrade packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import packageJson from './package.json'
 
-const { name, description, version } = packageJson
+const { description, version, productName } = packageJson
 
 const manifest = defineManifest({
-  name,
+  name: productName,
   description,
   version,
   manifest_version: 3,
